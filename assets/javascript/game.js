5216d1f77a69531a9f98087088bbff7c9b1bb988
Dloft <derek.lofton1@gmail.com>
2018-12-19 10:50:21 -0500

    firststep

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+var game = document.querySelector('main');
+
+var soul = 10;
+var reality = 15;
+var mind = 20;
+var space = 100;
+var time = 150; 
+var power = 200;
+
+document.getElementById( "" ).onclick = function() {
+    // img clicked
+
+
+
+};
