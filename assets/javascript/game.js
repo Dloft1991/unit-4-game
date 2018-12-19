ebddc6b6d2d3c522a1accc2623eaa8ba49b94051
Dloft <derek.lofton1@gmail.com>
2018-12-19 11:29:07 -0500

    updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,32 @@
 var game = document.querySelector('main');
 
-var soul = 10;
-var reality = 15;
-var mind = 20;
-var space = 100;
-var time = 150; 
-var power = 200;
+//random stone value ***
+var soul = Math.floor(Math.random() * (12 - 1) +1);
+var reality = Math.floor(Math.random() * (12 - 1) +1);
+var mind = Math.floor(Math.random() * (12 - 1) +1);
+var space = Math.floor(Math.random() * (12 - 1) +1);
+var time = Math.floor(Math.random() * (12 - 1) +1);
+var power = Math.floor(Math.random() * (12 - 1) +1);
 
-document.getElementById( "" ).onclick = function() {
-    // img clicked
+// counter and score ***
+//goal should be a randomly picked number
+// score will be sum of users imput
+var goal = Math.floor(Math.random() * (12 - 1) +1);
+var score = [];
 
+// wins and losses ***
+// will be ++; depending on if goal is reached or not then reset
+var wins = 0;
+var losses = 0;
 
+// start of game
+const startGame = function(){
 
-};
+    //put in here what you want to reset
+}
+
+// click functions
+$(".soul").on("click", function() {
+    alert("you did it");
+    
+});
